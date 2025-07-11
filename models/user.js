10e1819bd2f8427dd177f75f340c2288aed1d482
Manab Biswas <manabbiswas108108+github@gmail.com>
2025-07-11 12:58:49 +0530

    UI fix

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/minibackendProject')
 const userSchema = mongoose.Schema({
     username: String,
     name: String,
+    about: String,
     email: String,
     age: Number,
     password: String,
